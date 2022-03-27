0f89aa89b69a51dd0324659cdfad075c98ade17d
Adi Kancherla <findadit@gmail.com>
2022-03-27 17:31:09 -0500

    changes

--- contracts/core/CollectorsFeeManager.sol
+++ contracts/core/CollectorsFeeManager.sol
@@ -2,9 +2,8 @@
 pragma solidity ^0.8.0;
 
 import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
-import {IERC165, IERC2981} from '@openzeppelin/contracts/interfaces/IERC2981.sol';
+import {IERC165} from '@openzeppelin/contracts/interfaces/IERC165.sol';
 import {IFeeManager, FeeParty} from '../interfaces/IFeeManager.sol';
-import {IComplication} from '../interfaces/IComplication.sol';
 import {IOwnable} from '../interfaces/IOwnable.sol';
 import {IFeeRegistry} from '../interfaces/IFeeRegistry.sol';
 
@@ -19,27 +18,26 @@ contract CollectorsFeeManager is IFeeManager, Ownable {
   // ERC1155 interfaceID
   bytes4 public constant INTERFACE_ID_ERC1155 = 0xd9b67a26;
   FeeParty PARTY_NAME = FeeParty.COLLECTORS;
-  uint32 public MAX_COLL_FEE_BPS = 10000; // default
-  address public immutable collectorsFeeRegistry;
+  uint32 public MAX_COLL_FEE_BPS = 7500; // default
+  address public immutable COLLECTORS_FEE_REGISTRY;
 
-  event NewMaxBPS(uint32 newBps);
+  event NewMaxBPS(uint16 newBps);
 
   /**
    * @notice Constructor
    * @param _collectorsFeeRegistry address of the collectors fee registry
    */
   constructor(address _collectorsFeeRegistry) {
-    collectorsFeeRegistry = _collectorsFeeRegistry;
+    COLLECTORS_FEE_REGISTRY = _collectorsFeeRegistry;
   }
 
   /**
    * @notice Calculate collectors fees and get recipients
-   * @param complication address of the execution complication
    * @param collection address of the NFT contract
    * @param amount sale price
    */
   function calcFeesAndGetRecipients(
-    address complication,
+    address,
     address collection,
     uint256,
     uint256 amount
@@ -57,7 +55,7 @@ contract CollectorsFeeManager is IFeeManager, Ownable {
     uint256[] memory amounts;
 
     // check if collection is setup for fee share
-    (, recipients[0], , amounts[0]) = getCollectorsFeeInfo(complication, collection, amount);
+    (, recipients[0], , amounts[0]) = _getCollectorsFeeInfo(collection, amount);
 
     return (PARTY_NAME, recipients, amounts);
   }
@@ -100,45 +98,16 @@ contract CollectorsFeeManager is IFeeManager, Ownable {
     require(bps < MAX_COLL_FEE_BPS, 'bps too high');
 
     // setup
-    IFeeRegistry(collectorsFeeRegistry).registerFeeDestination(collection, msg.sender, feeDestination, bps);
+    IFeeRegistry(COLLECTORS_FEE_REGISTRY).registerFeeDestination(collection, msg.sender, feeDestination, bps);
   }
 
-  /**
-   * @notice Update owner of collector fee registry
-   * @dev Can be used for migration of this fee manager contract
-   * @param _owner new owner address
-   */
-  function updateOwnerOfCollectorsFeeRegistry(address _owner) external onlyOwner {
-    Ownable(collectorsFeeRegistry).transferOwnership(_owner);
-  }
+  // ============================================== INTERNAL FUNCTIONS ==============================================
 
-  function setMaxBpsForFeeShare(uint32 _maxBps) external onlyOwner {
-    MAX_COLL_FEE_BPS = _maxBps;
-    emit NewMaxBPS(_maxBps);
-  }
-
-  /**
-   * @notice Calculate protocol fee for an execution complication
-   * @param complication complication
-   * @param amount amount to transfer
-   */
-  function _calculateProtocolFee(address complication, uint256 amount) internal view returns (uint256) {
-    uint256 protocolFee = IComplication(complication).getProtocolFee();
-    return (protocolFee * amount) / 10000;
-  }
-
-  /**
-   * @notice Calculate collectors fee for a collection address and return info
-   * @param collection collection address
-   * @param amount amount
-   * @return setter, destination, bps and amount in this order
-   */
-  function getCollectorsFeeInfo(
-    address complication,
+   function _getCollectorsFeeInfo(
     address collection,
     uint256 amount
   )
-    public
+    internal
     view
     returns (
       address,
@@ -147,9 +116,39 @@ contract CollectorsFeeManager is IFeeManager, Ownable {
       uint256
     )
   {
-    (address setter, address destination, uint32 bps) = IFeeRegistry(collectorsFeeRegistry).getFeeInfo(collection);
-    uint256 protocolFee = _calculateProtocolFee(complication, amount);
-    uint256 collectorsFee = (bps * protocolFee) / 10000;
+    (address setter, address destination, uint32 bps) = IFeeRegistry(COLLECTORS_FEE_REGISTRY).getFeeInfo(collection);
+    uint256 collectorsFee = (bps * amount) / 10000;
     return (setter, destination, bps, collectorsFee);
+  }
+
+  // ============================================== VIEW FUNCTIONS ==============================================
+
+  /**
+   * @notice Calculate collectors fee for a collection address and return info
+   * @param collection collection address
+   * @param amount amount
+   * @return setter, destination, bps and amount in this order
+   */
+  function getCollectorsFeeInfo(
+    address collection,
+    uint256 amount
+  )
+    external
+    view
+    returns (
+      address,
+      address,
+      uint32,
+      uint256
+    )
+  {
+    return _getCollectorsFeeInfo(collection, amount);
+  }
+
+  // ===================================================== ADMIN FUNCTIONS =====================================================
+
+  function setMaxBpsForFeeShare(uint16 _maxBps) external onlyOwner {
+    MAX_COLL_FEE_BPS = _maxBps;
+    emit NewMaxBPS(_maxBps);
   }
 }
--- contracts/core/CollectorsFeeRegistry.sol
+++ contracts/core/CollectorsFeeRegistry.sol
@@ -9,6 +9,7 @@ import {IFeeRegistry} from '../interfaces/IFeeRegistry.sol';
  * @notice owned by CollectorsFeeManager; serves as a data registry
  */
 contract CollectorsFeeRegistry is IFeeRegistry, Ownable {
+  address COLLECTION_FEE_MANAGER;
   struct FeeInfo {
     address setter;
     address destination;
@@ -24,6 +25,8 @@ contract CollectorsFeeRegistry is IFeeRegistry, Ownable {
     uint32 bps
   );
 
+  event CollectionFeeManagerUpdated(address indexed manager);
+
   /**
    * @notice Update collectors fee for collection
    * @param collection address of the NFT contract
@@ -37,6 +40,7 @@ contract CollectorsFeeRegistry is IFeeRegistry, Ownable {
     address destination,
     uint32 bps
   ) external override onlyOwner {
+    require(msg.sender == COLLECTION_FEE_MANAGER, 'Collection Registry: Only collection fee manager');
     _collectorsFeeInfo[collection] = FeeInfo({setter: setter, destination: destination, bps: bps});
     emit CollectorsFeeUpdate(collection, setter, destination, bps);
   }
@@ -60,5 +64,12 @@ contract CollectorsFeeRegistry is IFeeRegistry, Ownable {
       _collectorsFeeInfo[collection].destination,
       _collectorsFeeInfo[collection].bps
     );
+  }
+
+  // ===================================================== ADMIN FUNCTIONS =====================================================
+
+  function updateCollectionFeeManager(address manager) external onlyOwner {
+    COLLECTION_FEE_MANAGER = manager;
+    emit CollectionFeeManagerUpdated(manager);
   }
 }
