bf646d0455ec018cbea141df742c66504fe5632d
Adi Kancherla <findadit@gmail.com>
2022-03-31 17:14:13 -0500

    new trading rewards contract, royalty splitter missed prev push

---
+++ contracts/interfaces/IInfinityTradingRewards.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Duration} from '../interfaces/IStaker.sol';
+
+interface IInfinityTradingRewards {
+  function updateRewards(
+    address[] calldata sellers,
+    address[] calldata buyers,
+    address[] calldata currencies,
+    uint256[] calldata amounts
+  ) external;
+
+  function claimRewards(
+    address destination,
+    address currency,
+    uint256 amount
+  ) external;
+
+  function stakeInfinityRewards(uint256 amount, Duration duration) external;
+}
