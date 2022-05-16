c89a1a1c94591a0bfd6101dd168ac9c42586db44
nneverlander <findadit@gmail.com>
2022-05-16 14:59:51 +0530

    solc version locked

--- contracts/core/InfinityExchange.sol
+++ contracts/core/InfinityExchange.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity 0.8.9;
 
 import {OrderTypes} from '../libs/OrderTypes.sol';
 import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
